9f81f75ebb68833d33b3282c7cdd649f20cc6f4c
José Guilherme Sório <joseguilhermesorio@gmail.com>
2023-08-22 23:44:07 -0300

    feat: create relationships and ensure using uuids on models

--- app/Models/Check.php
+++ app/Models/Check.php
@@ -2,12 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Check extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
 
     protected $fillable = ['status_code', 'response_body'];
+
+    public function endpoint(): BelongsTo
+    {
+        return $this->belongsTo(Endpoint::class);
+    }
 }
